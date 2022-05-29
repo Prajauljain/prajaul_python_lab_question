78fd2133e74104e2bc81a1f143cacb65fcde8e03
Prajauljain <104970887+Prajauljain@users.noreply.github.com>
2022-05-29 05:19:56 -0800

    Create check_keys_equal_or_not_Dictionary7.py

---
+++ check_keys_equal_or_not_Dictionary7.py
@@ -0,0 +1,13 @@
+d = {}
+n = int(input())
+for i in range (n):
+    a = int(input())
+    b = int(input())
+    d.update({a:b})
+print(d)
+if (a==b):
+    print("Yes")
+else :
+    print("No")
+© 2022 GitHub, Inc.
+Terms
