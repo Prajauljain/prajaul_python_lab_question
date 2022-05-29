0701e03774d1ffd1117f74d6254998a470209d24
Prajauljain <104970887+Prajauljain@users.noreply.github.com>
2022-05-29 00:42:05 -0800

    Create key_is_in_dictinary_or_not_dictionary6.py

---
+++ key_is_in_dictinary_or_not_dictionary6.py
@@ -0,0 +1,14 @@
+d={}
+x=int(input())
+for i  in range (x):
+    k=int(input())
+    v=int(input())
+    d.update({k:v})
+key=int(input())
+for i in d.keys():
+    if i==key:
+        print('present')
+        break
+    else:
+        print('not  present')
+        break
